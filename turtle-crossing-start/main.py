3d02c3858f6c571b4fbf89a291595ddbfe8d642b
Nike Balonga <rnnbalongapro@gmail.com>
2023-02-06 21:03:09 +0900

    Start creating Turtle Crossing Project

---
+++ turtle-crossing-start/main.py
@@ -0,0 +1,33 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+#Create Turtle
+#   Starts at the middle of the screen
+#   Will only go along the y-axis. User clicks up to go up and down to go down
+#   Will go back to the original starting position when it gets to the edge of the screen
+
+#Create Cars
+#   Random Turtle
+#   All fixed size - think of paddle from Pong project
+#   random colors
+#   Will randomly be generated in the middle of the screen
+#   Will move along the x-axis
+#   Will double in speed every time turtle hits the top of the screen
+
+#Screen
+#   Will indicate which level user is currently in
+#   Level will update every time turtle hits the top edge of the screen
+#   Game will end once turtle gets hit by a car.
+#   Shows 'Game Over' message once game ends as stated above
