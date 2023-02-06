461827e5e36af5d016276a8afdcbda1e744bead9
Nike Balonga <rnnbalongapro@gmail.com>
2023-02-06 20:45:47 +0900

    Build snake-game main file

--- snake-game/main.py
+++ snake-game/main.py
@@ -1,29 +1,50 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
+import time
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Bayawak")
+screen.tracer(0)
 
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
-
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
-
-
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-
+screen.listen()
+screen.onkey(snake.go_up, "w")
+screen.onkey(snake.go_down, "s")
+screen.onkey(snake.go_left, "a")
+screen.onkey(snake.go_right, "d")
 
 game_is_on = True
 
 while game_is_on:
-    for seg in segments:
-        seg.forward(20)
 
+    #To make the animation of the snake moving forward smooth
+    screen.update()
+    time.sleep(0.08)
+
+    snake.move()
+
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.add_score()
+
+    #Detect collision with food
+    if snake.head.xcor() > 290 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    #Detect collision with food
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
