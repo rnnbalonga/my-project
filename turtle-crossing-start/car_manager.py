094e7e91cba48aaa6f7943d92c8b8ea85c540b5e
Nike Balonga <rnnbalongapro@gmail.com>
2023-02-09 22:05:12 +0900

    Add global variables CAR_END & RIGHT_HAND_START. Also add method go_right

--- turtle-crossing-start/car_manager.py
+++ turtle-crossing-start/car_manager.py
@@ -4,7 +4,8 @@ import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
-
+CAR_END: -310
+RIGHT_HAND_START = 300
 
 class CarManager(Turtle):
     def __init__(self):
@@ -21,3 +22,9 @@ class CarManager(Turtle):
         """
         self.setposition(random.randrange(-300,300),random.randrange(-220,230))
 
+    def go_right(self):
+        """
+        This function will make the car who reaches CAR_END go back to RIGHT_HAND_START (or the right side of the screen).
+        """
+        if self.xcor == CAR_END:
+            self.goto(RIGHT_HAND_START,random.randrange(-220,230))
