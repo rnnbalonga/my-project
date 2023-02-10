e61a436162e4b56bfadc1989ceb334835c3fb5a4
Nike Balonga <rnnbalongapro@gmail.com>
2023-02-10 13:56:21 +0900

    Create method update_level

--- turtle-crossing-start/scoreboard.py
+++ turtle-crossing-start/scoreboard.py
@@ -12,10 +12,17 @@ class Scoreboard(Turtle):
         #Set spawn point
         self.goto(-270,260)
         self.hideturtle()
-        self.level = 0
+        self.level = 1
 
     def start_level(self):
         """
         This function will write the player level in the screen.
         """
         self.write(f"Current level: {self.level}",font= FONT)
+
+    def update_level(self):
+        """
+        This function will update the level of the player.
+        """
+        self.level += 1
+        self.start_level()
