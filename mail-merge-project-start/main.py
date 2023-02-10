3ed2c180c9c3eee03694c133b5dea78dc7436776
Nike Balonga <rnnbalongapro@gmail.com>
2023-02-10 22:29:59 +0900

    Start using starting_letter.txt and create a list of lines from the text

--- mail-merge-project-start/main.py
+++ mail-merge-project-start/main.py
@@ -1,9 +1,23 @@
 #Open the invited_names text file
 with open(r"Input\Names\invited_names.txt") as names:
+
+    #Create a list of names using readlines()
     name_list = names.readlines()
+
+    #Capture each individual name from name_list
     for name_index in range(0, len(name_list) -1):
         name = name_list[name_index]
-        print(name)
+        
+        #Use starting_letter.txt
+        with open("Input\Letters\starting_letter.txt", "r") as letter:
+            #Create a list of the lines in text
+            letter_line_list = letter.readlines()
+
+            #Capture each line from the letter
+            for letter_index in range(0, len(letter_line_list) -1):
+                letter_line = letter_line_list[letter_index]
+                print(letter_line)
+
 
 
 
