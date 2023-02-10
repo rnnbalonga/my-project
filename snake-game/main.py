6064183998f3dfc7310f0686ae17945e24de636d
Nike Balonga <rnnbalongapro@gmail.com>
2023-02-10 15:19:01 +0900

    Remove all instances of game_is_on = False & scoreboard.game_over

--- snake-game/main.py
+++ snake-game/main.py
@@ -38,13 +38,13 @@ while game_is_on:
 
     #Detect collision with food
     if snake.head.xcor() > 290 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        
 
     #Detect collision with food
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            
 
 screen.exitonclick()
