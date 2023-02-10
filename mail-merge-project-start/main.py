ad2eb4c572a3a3e280a79dbcb2343bb99f777dab
Nike Balonga <rnnbalongapro@gmail.com>
2023-02-10 21:53:00 +0900

    Create a list of names using readlines() and capture each individual name from name_list

--- mail-merge-project-start/main.py
+++ mail-merge-project-start/main.py
@@ -1,3 +1,12 @@
+#Open the invited_names text file
+with open(r"Input\Names\invited_names.txt") as names:
+    name_list = names.readlines()
+    for name_index in range(0, len(name_list) -1):
+        name = name_list[name_index]
+        print(name)
+
+
+
 #TODO: Create a letter using starting_letter.txt 
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
