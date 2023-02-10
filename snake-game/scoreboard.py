c458dbff4a2d0a92bef06076488fe113f033f191
Nike Balonga <rnnbalongapro@gmail.com>
2023-02-10 16:33:55 +0900

    Make self.high_score read info from txt file

--- snake-game/scoreboard.py
+++ snake-game/scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
         #initialize superclass
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("score.txt", "w") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0,270)
@@ -21,6 +22,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("score.txt","w") as data:
+                data.write(f"{self.high_score}")
             self.score = 0
             self.clear()
             self.update_score()   
