1bc6b4f0f5685834fc65ff8f5a2dcb45f87d145b
Nike Balonga <rnnbalongapro@gmail.com>
2023-02-10 16:00:20 +0900

    Improve comments and add snake.reset to collision with wall if statement

--- snake-game/main.py
+++ snake-game/main.py
@@ -36,12 +36,13 @@ while game_is_on:
         snake.extend()
         scoreboard.add_score()
 
-    #Detect collision with food
+    #Detect collision with wall
     if snake.head.xcor() > 290 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         scoreboard.reset()
-        
+        snake.reset()
 
-    #Detect collision with food
+
+    #Detect collision with body
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             scoreboard.reset()
