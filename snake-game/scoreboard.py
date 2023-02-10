8de9553e37d2082550f5c5c93cc4cff042763429
Nike Balonga <rnnbalongapro@gmail.com>
2023-02-10 15:58:45 +0900

    Add mechanism to update high score in method 'reset

--- snake-game/scoreboard.py
+++ snake-game/scoreboard.py
@@ -16,17 +16,18 @@ class Scoreboard(Turtle):
         self.update_score()
         
     def update_score(self):
-        self.write(f"Score: {self.score}", align=ALIGN, font = FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGN, font = FONT)
 
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.score = 0
+            self.clear()
+            self.update_score()   
 
     # def game_over(self):
     #     self.goto(0,0)
     #     self.write(f"Game Over!", align=ALIGN, font = FONT)
-
-   
 
     def add_score(self):
         self.score += 1
