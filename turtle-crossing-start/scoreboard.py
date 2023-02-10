47d5b58b6ccca3e81334603a9c1ded5f7f537c9f
Nike Balonga <rnnbalongapro@gmail.com>
2023-02-10 13:43:04 +0900

    Hide scoreboard turtle

--- turtle-crossing-start/scoreboard.py
+++ turtle-crossing-start/scoreboard.py
@@ -1,16 +1,14 @@
 from turtle import Turtle
 
 FONT = ("Courier", 24, "normal")
-
-
 ALIGN = "center"
-FONT = ("Arial", 16, "normal")
 
 class Scoreboard(Turtle):
     def __init__(self):
         #initialize superclass
         super().__init__()
         self.score = 0
-        self.color("white")
+        self.color("black")
         self.penup()
         self.goto(-270,270)
+        self.hideturtle()
