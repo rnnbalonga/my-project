79bd2e6953b699b47f0cf19150dfff860925ec38
Nike Balonga <rnnbalongapro@gmail.com>
2023-02-10 13:37:25 +0900

    Create and initialize class 'Scoreboard'

--- turtle-crossing-start/scoreboard.py
+++ turtle-crossing-start/scoreboard.py
@@ -1,5 +1,16 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+ALIGN = "center"
+FONT = ("Arial", 16, "normal")
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        #initialize superclass
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(-270,270)
