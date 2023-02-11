c1b54377797d5eaacc56d83a7fa433017dfde924
Nike Balonga <rnnbalongapro@gmail.com>
2023-02-11 22:15:23 +0900

    Add the temperature found in each row inside the weather_data csv into the list 'temperatures'

--- weather/main.py
+++ weather/main.py
@@ -1,8 +1,15 @@
 import csv
 
 with open("weather_data.csv", "r") as weather_file:
-    csv.reader(weather_file)
+    data = csv.reader(weather_file)
     #Get the temperatures in each line of the file as an integer and add them to the list: temperatures
     temperatures = []
-    for row in weather_file:
-        print(row)
+    #Loop through each row in the file
+    for row in data:
+        #Select temperatures which is in index 1 of each row.
+        #Add an if statement to exclude the word temp
+        if row[1] != "temp":
+            #Append index [1] of each row to the temperature list as an integer
+            temperatures.append(int(row[1]))
+    print(temperatures)
+            
