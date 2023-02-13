9395d3123addfe138008f9e28b33d14838ed2388
Nike Balonga <rnnbalongapro@gmail.com>
2023-02-13 21:17:37 +0900

    Get mean of series 'temp' using pandas

--- weather/main.py
+++ weather/main.py
@@ -21,8 +21,12 @@ data = pandas.read_csv("weather_data.csv")
 
 #Convert dataframe to dictionary
 data_dict = data.to_dict()
-print(data_dict)
+# print(data_dict)
 
 #Convert series to list
 temp_list = data["temp"].to_list()
-print(temp_list)
+# print(temp_list)
+
+#Get mean of series 'temp' using pandas
+temp_mean = data["temp"].mean()
+print(temp_mean)
