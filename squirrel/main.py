31eff9757c26d5aa14f3d9170211b1b338c2299e
Nike Balonga <rnnbalongapro@gmail.com>
2023-02-15 21:56:53 +0900

    Reorganize code and get the count for black and red squirrels

--- squirrel/main.py
+++ squirrel/main.py
@@ -7,13 +7,16 @@ squirrel_raw = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.cs
 squirrel_unique_colors = squirrel_raw["Primary Fur Color"].unique()
 print(squirrel_unique_colors)
 
-#Get a hold of a list of all squirrels with Gray as their primary fur color
-gray_squirrel = squirrel_raw[squirrel_raw["Primary Fur Color"] == "Gray"]
-#Get the length of the grey squirrel list
-gray_squirrel_count = int(len(gray_squirrel))
-print(gray_squirrel_count)
 
-squirrel_new = {
-    'Fur Color': [],
-    'Count': []
-}
+#Get the length of the grey squirrel list
+gray_squirrel_count = int(len(squirrel_raw[squirrel_raw["Primary Fur Color"] == "Gray"]))
+red_squirrel_count = int(len(squirrel_raw[squirrel_raw["Primary Fur Color"] == "Cinnamon"]))
+black_squirrel_count = int(len(squirrel_raw[squirrel_raw["Primary Fur Color"] == "Black"]))
+print(gray_squirrel_count)
+print(red_squirrel_count)
+print(black_squirrel_count)
+
+# squirrel_new = {
+#     'Fur Color': [],
+#     'Count': []
+# }
