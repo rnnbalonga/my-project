7f437b5e011b8a8968b7ab58c01f33a7f073ce35
Nike Balonga <rnnbalongapro@gmail.com>
2023-02-15 22:39:03 +0900

    Get coordinates on mouse click

--- us-states-game/us-states-game-start/us-states-game-start/main.py
+++ us-states-game/us-states-game-start/us-states-game-start/main.py
@@ -10,5 +10,12 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+#Set up getting coordinates via mouseclick on screen
+def get_mouse_click_coor(x, y):
+    print(x, y)
+
+turtle.onscreenclick(get_mouse_click_coor)
+
+
 screen.mainloop()
 
