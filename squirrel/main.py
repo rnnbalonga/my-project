7b81cef679e60111d15c59d6a4964863f89fb5bc
Nike Balonga <rnnbalongapro@gmail.com>
2023-02-15 21:48:33 +0900

    Change pd.DataFrame to pd.read_csv in line 4

--- squirrel/main.py
+++ squirrel/main.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 #Create a DataFrame out of the 2018 Census
-squirrel_raw = pd.DataFrame("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+squirrel_raw = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
 
 squirrel_unique_colors = squirrel_raw["Primary Fur Color"].unique()
-print(squirrel_unique_colors)
+
 
 squirrel_new = {
     'Fur Color': [],
