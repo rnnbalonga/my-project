4c90e098dcdcbfc60d130aea32ed41e976998e7e
Nike Balonga <rnnbalongapro@gmail.com>
2023-02-15 22:37:52 +0900

    Set up screen and add image as background

---
+++ us-states-game/us-states-game-start/us-states-game-start/main.py
@@ -0,0 +1,14 @@
+import turtle
+import pandas
+
+#Screen Set Up
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+
+#Set up image as background
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+screen.mainloop()
+
