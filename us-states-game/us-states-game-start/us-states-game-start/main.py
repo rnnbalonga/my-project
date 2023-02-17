a50f5ad6138722c3a3f9009355e20636b72461f2
Nike Balonga <rnnbalongapro@gmail.com>
2023-02-17 22:17:27 +0900

    Build out us-states-game

--- us-states-game/us-states-game-start/us-states-game-start/main.py
+++ us-states-game/us-states-game-start/us-states-game-start/main.py
@@ -10,14 +10,43 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+df = pandas.read_csv("50_states.csv")
+states_list = df.state.to_list()
+#Create list of the states correctly guessed by user
+user_guess = []
+
+#Keep the screen open while guessed states < 50
+while len(user_guess) < 50:
+    answer_state = screen.textinput(title=f"{len(user_guess)}/50 States Correct", prompt="What's another state's name?").title()
+
+    #Create mechanism to force end game
+    if answer_state == "Exit":
+        #Get the difference between the states_list and user_guess
+        unmemorized_states = list(set(states_list) - set(user_guess))
+        df_new = pandas.DataFrame(unmemorized_states)
+        df_new.to_csv("Unmemorized States")
+        break
+    if answer_state in states_list:
+        #Append correct answer to user_guess
+        user_guess.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        #Grab row data
+        state_data = df[df.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+    
+
+
+
 
 #Set up getting coordinates via mouseclick on screen
-def get_mouse_click_coor(x, y):
-    print(x, y)
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
 
-turtle.onscreenclick(get_mouse_click_coor)
+# turtle.onscreenclick(get_mouse_click_coor)
 
 
-
-screen.mainloop()
+# screen.mainloop()
 
