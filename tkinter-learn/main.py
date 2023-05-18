32760134e7ef5cb28be2ab433016b3303ab1eb18
Nike Balonga <rnnbalongapro@gmail.com>
2023-05-18 21:53:48 +0900

    Add padding and use grid to layout items

--- tkinter-learn/main.py
+++ tkinter-learn/main.py
@@ -6,11 +6,14 @@ window = Tk()
 window.title("First GUI")
 #Set window dimensions
 window.minsize(width=500, height=300)
+#Add padding to window
+window.config(padx=20, pady=20)
 
 #Create Label
-label_1= Label(text="This is my first label", font=("Arial", 24))
-#Make label appear on the window
-label_1.pack()
+label_1= Label(text="New Text", font=("Arial", 24))
+#Make label appear on the window using 'grid'
+label_1.grid(column=0, row=0)
+label_1.config(padx=20, pady=20)
 
 #Function to listen for button click
 def button_click():
@@ -20,13 +23,16 @@ def button_click():
 
 #Create button
 button = Button(text="Don't click", command=button_click)
-button.pack()
+button.grid(column=1, row=1)
+
+#New Button
+button_2 = Button(text="This is the new button")
+button_2.grid(column=2, row=0)
+
 
 #Create Input Field
 input = Entry(width=10)
-input.pack()
-#Get the input 
-
+input.grid(column=3, row=3)
 
 
 #This line will make the window appear indefinitely
