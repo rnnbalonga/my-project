8d48eaab51f70ad8ec041bb06259e83db69794be
Nike Balonga <rnnbalongapro@gmail.com>
2023-05-20 17:17:25 +0900

    Remove window size. Create canvas, add image, and adjust dimensions.

--- password-manager/main.py
+++ password-manager/main.py
@@ -2,8 +2,14 @@ from tkinter import *
 
 #Window
 window = Tk()
-window.minsize(width=200, height=200)
 window.config(padx=20, pady=20)
 window.title("Password Manager")
+
+#Canvas
+canvas = Canvas(width=210, height=210, highlightthickness=0)
+#Add image to canvas
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.pack()
 
 window.mainloop()
