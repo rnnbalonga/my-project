e970238cf206221998db025253d2a684c207b6c4
Nike Balonga <rnnbalongapro@gmail.com>
2023-05-21 23:52:34 +0900

    Import pyperclip. First working version

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
+import pyperclip
 
 #------------- PASSWORD GENERATOR -------------#
 
@@ -24,6 +25,7 @@ def generate_password():
     password = "".join(password_list)
 
     password_input.insert(0, password)
+    pyperclip.copy(password)
 
 
 #------------- SAVE PASSWORD -------------#
