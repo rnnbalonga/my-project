6abf6db2a243a7a651bcc50755da7d7aff310078
Nike Balonga <rnnbalongapro@gmail.com>
2023-05-22 22:49:18 +0900

    Use dictionary comprehension to make dictionary of phonetic code

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,5 +1,13 @@
+import pandas as p
+
 #TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+# {"A": "Alfa", "B": "Bravo"}
+
+
+
+phonetic_alphabet_df = p.read_csv("nato_phonetic_alphabet.csv")
+
+phonetic_code = {row["letter"]:row["code"] for index, row in phonetic_alphabet_df.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
