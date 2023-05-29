e0244cfd3e484905592a03743b7995d1b4d3bc19
Nike Balonga <rnnbalongapro@gmail.com>
2023-05-29 22:29:24 +0900

    Set up image as a canvas

--- flash-card-project-start/main.py
+++ flash-card-project-start/main.py
@@ -12,6 +12,10 @@ window.title("Memory Trainer")
 #Canvas
 canvas = Canvas(width=800, height=526)
 canvas.grid(row=0,column=0, columnspan=2)
+front_card_image = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=front_card_image)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0)
 
 #Title Text
 title = Label()
