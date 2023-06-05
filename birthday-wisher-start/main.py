6d928f1350953a1c8cb29acff5c86002f66e7c04
Nike Balonga <rnnbalongapro@gmail.com>
2023-06-05 23:42:09 +0900

    Add trial code to send emails every Monday

--- birthday-wisher-start/main.py
+++ birthday-wisher-start/main.py
@@ -1,11 +1,28 @@
 import smtplib
+import datetime as dt
 
 my_email = "ruben.lingito@gmail.com"
 password = "sxznwsoaehfcsnpm"
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email, 
-                        to_addrs="rara.huhu@yahoo.com", 
-                        msg="Subject: Happy Birthday!\n\nThis is my attempt at sending emails via python")
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(from_addr=my_email, 
+#                         to_addrs="fightmaregaming@gmail.com", 
+#                         msg="Subject: Happy Birthday!\n\nThis is my attempt at sending emails via python")
+
+
+now = dt.datetime.now()
+day_of_week = now.weekday()
+
+def send_quote():
+    if day_of_week == 0:
+        with open("quotes.txt") as quote_file:
+            quote = quote_file.readlines()
+            quote_number = 0 
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email, 
+                                to_addrs="fightmaregaming@gmail.com", 
+                                msg=quote[quote_number])
