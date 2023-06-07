57dd27a237d691e0f111273807a5a96d78d99a23
Nike Balonga <rnnbalongapro@gmail.com>
2023-06-07 10:51:49 +0900

    Add list of email templates. Check if there's a matching month in dictionary

--- birthday-wisher-start/main.py
+++ birthday-wisher-start/main.py
@@ -12,6 +12,7 @@ password = "sxznwsoaehfcsnpm"
 
 df = pd.read_csv("email-list.txt")
 email_list = df.to_dict('records')
+email_templates = ["email-1.txt", "email-2.txt", "email-3.txt"]
 
 #Check today's date
 
@@ -19,4 +20,17 @@ today = dt.datetime.now()
 today_month = today.month
 today_day = today.day
 
+#Check if there's a matching month in the dictionary
+
+for i in range(0,2):
+    if today_month == email_list[i]['month']:
+        if today_day == email_list[i]['day']:
+            print("HEYYYYY")
+            with open()
+            # with smtplib.SMTP("smtp.gmail.com") as connection:
+            #     connection.starttls()
+            #     connection.login(user=my_email, password=password)
+            #     connection.sendmail(from_addr=my_email, 
+            #                         to_addrs="fightmaregaming@gmail.com", 
+            #                         msg=f"Subject:HAPPY BIRTHDAY TO YOU!\n\n{random.choice(quote)}")
 
