fbace0a8daae14adb569160f6e9b63352b97abe4
Nike Balonga <rnnbalongapro@gmail.com>
2023-06-07 10:27:04 +0900

    Create a list of dictionary out of dataframe from email-list.txt

--- birthday-wisher-start/main.py
+++ birthday-wisher-start/main.py
@@ -1,9 +1,17 @@
 import random
 import smtplib
 import datetime as dt
+import pandas as pd
 
 my_email = "ruben.lingito@gmail.com"
 password = "sxznwsoaehfcsnpm"
 
 
 
+#create a list of dictionaries from email-list file
+
+df = pd.read_csv("email-list.txt")
+email_list = df.to_dict('record')
+
+
+#If date_today == birthday send email
