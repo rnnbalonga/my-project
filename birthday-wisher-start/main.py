d538e7226b3116dd24d2ba382de5faec24150b87
Nike Balonga <rnnbalongapro@gmail.com>
2023-06-07 11:55:24 +0900

    Correct code by simplifying email_content line and creating final_email to be sent. Also add a line to schedule code to run at 12:00 daily.

--- birthday-wisher-start/main.py
+++ birthday-wisher-start/main.py
@@ -2,11 +2,11 @@ import random
 import smtplib
 import datetime as dt
 import pandas as pd
+import time
+import schedule
 
 my_email = "ruben.lingito@gmail.com"
-password = "sxznwsoaehfcsnpm"
-
-
+password = "kizszdeyikbdrhig"
 
 #create a list of dictionaries from email-list file
 
@@ -26,16 +26,20 @@ def greet():
         if today_month == email_list[i]['month']:
             if today_day == email_list[i]['day']:
                 name = email_list[i]['name']
+                name_email = email_list[i]['email']
                 with open(random.choice(email_templates), 'r') as email_file:
-                    email_content = email_file.readlines()
-                    email_greeting = email_content[0].replace("[NAME]", name)
-                    email_content[0] = email_greeting
-                    print(email_content)
+                    email_content = email_file.read()
+                    final_email = email_content.replace("[NAME]", name)
                 with smtplib.SMTP("smtp.gmail.com") as connection:
                     connection.starttls()
                     connection.login(user=my_email, password=password)
                     connection.sendmail(from_addr=my_email, 
-                                        to_addrs="fightmaregaming@gmail.com", 
-                                        msg=f"Subject:HAPPY BIRTHDAY TO YOU!\n\n{email_content}")
+                                        to_addrs=name_email, 
+                                        msg=f"Subject:HAPPY BIRTHDAY TO YOU!\n\n{final_email}")
+
                     
-greet()
+schedule.every().day.at("12:00").do(greet)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
