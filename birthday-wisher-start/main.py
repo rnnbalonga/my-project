5f1a5ebe565193f3f368e0ce11573c2ac8342165
Nike Balonga <rnnbalongapro@gmail.com>
2023-06-07 10:38:24 +0900

    Add variables to capture today's date, month, and day

--- birthday-wisher-start/main.py
+++ birthday-wisher-start/main.py
@@ -11,7 +11,12 @@ password = "sxznwsoaehfcsnpm"
 #create a list of dictionaries from email-list file
 
 df = pd.read_csv("email-list.txt")
-email_list = df.to_dict('record')
+email_list = df.to_dict('records')
+
+#Check today's date
+
+today = dt.datetime.now()
+today_month = today.month
+today_day = today.day
 
 
-#If date_today == birthday send email
