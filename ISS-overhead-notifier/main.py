350721dd8b4fa1e863b4e4348926db81f87e1b77
Nike Balonga <rnnbalongapro@gmail.com>
2023-06-08 17:11:58 +0900

    Add email settings

--- ISS-overhead-notifier/main.py
+++ ISS-overhead-notifier/main.py
@@ -6,6 +6,7 @@ MY_LONG = 35.5223
 MY_LAT = 139.7308
 
 today = dt.datetime.now()
+current_hour = today.hour
 
 #Sunrise & Sunset API
 parameters = {
@@ -29,18 +30,38 @@ iss_data = iss_response.json()
 iss_longitude = float(iss_data['iss_position']['longitude'])
 iss_latitude = float(iss_data['iss_position']['latitude'])
 
+
+##### FUNCTIONS #####
+
 #Check if ISS postion is close to my current location
 
 def iss_location_check():
     '''
-    This function checks if the ISS is within 5 deg of my coordinates
+    This function checks if the ISS is within 5 deg of my coordinates.
     '''
     long_diff = abs(MY_LONG - iss_longitude)
     lat_diff = abs(MY_LAT - iss_latitude)
     if long_diff <= 5 and lat_diff <=5:
-        print("Heyyyyy")
+        return True
     else:
-        print("welp")
+        return False
+
+#Check if it's currently dark
+
+def check_dark():
+    '''
+    This function checks if current hour is between sunset and sunrise.
+    '''
+    if current_hour < sunrise or current_hour > sunset:
+        return True
+    else:
+        return False
+
+def send_mail():
+    '''
+    If both iss_location_check and check_dark return TRUE, an email will be sent.
+    '''
 
 
 iss_location_check()
+check_dark()
