900bb026093fb52d435eb91bb85fa2a4cfc2773d
Nike Balonga <rnnbalongapro@gmail.com>
2023-06-08 16:21:22 +0900

    Change parameters and create sunrise and sunset variables

--- ISS-overhead-notifier/main.py
+++ ISS-overhead-notifier/main.py
@@ -10,11 +10,18 @@ today = dt.datetime.now()
 #'lng' and 'lat' are currently set to Kawasaki's coordinates. Consider creating a constant for this instead.
 
 parameters = {
-    'lng' : 35.53,
-    'lat' : 139.70,
+    'lng' : 35.5223,
+    'lat' : 139.7308,
+    'formatted' : 0,
 }
 
 response = requests.get(url, params=parameters)
 response.raise_for_status()
 data = response.json()
-print(data)
+sunrise = data['results']['sunrise'].split("T")[1].split(":")[0]
+sunset = data['results']['sunset'].split("T")[1].split(":")[0]
+
+
+print(sunrise)
+print(sunset)
+print(today.hour)
