8992a6bf66f555cf6c3b129143c214752c3647c9
Nike Balonga <rnnbalongapro@gmail.com>
2023-06-08 15:29:09 +0900

    Create starting parameters for API

--- ISS-overhead-notifier/main.py
+++ ISS-overhead-notifier/main.py
@@ -1,7 +1,18 @@
 import requests
 import json
+import datetime as dt
 
-url = 'https://api.sunrise-sunset.org/json.'
+url = 'https://api.sunrise-sunset.org/json'
+
+today = dt.datetime.now()
+
+#Set parameters for API
+
+parameters = {
+    lng: 35.53,
+    lat: 139.70,
+    date: today,
+}
 
 response = requests.get(url)
 
