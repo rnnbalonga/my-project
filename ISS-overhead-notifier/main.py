1c086955ed88634212b166d34ac7ad7b45dbb379
Nike Balonga <rnnbalongapro@gmail.com>
2023-06-08 15:44:40 +0900

    Simplify code

--- ISS-overhead-notifier/main.py
+++ ISS-overhead-notifier/main.py
@@ -7,14 +7,14 @@ url = 'https://api.sunrise-sunset.org/json'
 today = dt.datetime.now()
 
 #Set parameters for API
+#'lng' and 'lat' are currently set to Kawasaki's coordinates. Consider creating a constant for this instead.
 
 parameters = {
     'lng' : 35.53,
     'lat' : 139.70,
-    'date': today,
 }
 
-response = requests.get(url)
-print(response)
-
-
+response = requests.get(url, params=parameters)
+response.raise_for_status()
+data = response.json()
+print(data)
