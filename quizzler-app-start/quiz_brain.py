a8915357f1ab1f5491e98b0a70f78bfa427fd745
Nike Balonga <rnnbalongapro@gmail.com>
2023-06-12 00:34:56 +0900

    Add 'str' type hint to user_answer input in check_answer method

--- quizzler-app-start/quiz_brain.py
+++ quizzler-app-start/quiz_brain.py
@@ -18,7 +18,7 @@ class QuizBrain:
         return f"Q.{self.question_number}: {q_text}"
         # self.check_answer(user_answer)
 
-    def check_answer(self, user_answer):
+    def check_answer(self, user_answer: str):
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
