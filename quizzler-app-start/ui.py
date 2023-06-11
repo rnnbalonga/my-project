90a9c7eabababe9c1e3c06241e06c695fdbf0322
Nike Balonga <rnnbalongapro@gmail.com>
2023-06-11 23:28:26 +0900

    Create label for user score

--- quizzler-app-start/ui.py
+++ quizzler-app-start/ui.py
@@ -7,5 +7,11 @@ class QuizUI:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quiz App")
-        self.window.config(bg=THEME_COLOR)
+        self.window.minsize(width=400,height=600)
+        self.window.config(bg=THEME_COLOR, padx=20, pady=20)
+
+        self.score_label = Label(text="Score: ", font=("Arial", 12), bg=THEME_COLOR, fg="white")
+        self.score_label.grid(row=0, column=1)
+
+
         self.window.mainloop()
