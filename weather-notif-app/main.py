57834eb4b6b8bea21024af2f1185ec660ee5c606
Nike Balonga <rnnbalongapro@gmail.com>
2023-06-12 11:04:13 +0900

    Revise 'appid' key and add exclude parameter

--- weather-notif-app/main.py
+++ weather-notif-app/main.py
@@ -5,7 +5,8 @@ URL = "https://api.openweathermap.org/data/3.0/onecall?"
 parameters = {
     'lng' : 139.7029,
     'lat' : 35.5308,
-    'app_id': "98f54ab1014ca867c1a5d590939ca4d9"
+    'appid': "98f54ab1014ca867c1a5d590939ca4d9"
+    'exclude' "currently,minutely,daily"
 }
 
 response = requests.get(URL, params=parameters)
