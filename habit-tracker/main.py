68379ea05088ab7c85eea92bdae3b11d8ac27f13
Nike Balonga <rnnbalongapro@gmail.com>
2023-06-14 21:14:37 +0900

    Clean up some code and add graph_config

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -1,13 +1,27 @@
 import requests
 
+USERNAME = "nikebalonga"
+TOKEN = "29b2f8302a75c6bbbccc0867ba79ad8e"
+
 pixela_endpoint = "https://pixe.la/v1/users"
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 user_params = {
-    "token": "29b2f8302a75c6bbbccc0867ba79ad8e",
-    "username": "nikebalonga",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
-response = requests.post(url=pixela_endpoint,json=user_params)
+graph_config = {
+    "id": "q1c1b0f4b2302ca3",
+    "name": "Coding Habit",
+    "unit": "Days",
+    "type": "int",
+    "color": "shibafu",
+}
+# response = requests.post(url=pixela_endpoint,json=user_params)
+# print(response.text)
+
+response = requests.post(url=graph_endpoint,json=graph_config)
 print(response.text)
