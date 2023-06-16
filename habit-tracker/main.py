e2697398fecd5f2bfcc99240bfe112a6c4ab38e7
Nike Balonga <rnnbalongapro@gmail.com>
2023-06-16 21:17:43 +0900

    Revise pixel_create_enpoint var name and string. Add parameters for pixel_create_config

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -6,7 +6,7 @@ GRAPH_ID = "q1c1b0f4b2302ca3"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
-post_endpoint = f"/v1/users/{USERNAME}/graphs/{GRAPH_ID}"
+pixel_create_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
 
 user_params = {
     "token": TOKEN,
@@ -27,8 +27,17 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
+pixel_create_config = {
+    "date" : "20230616",
+    "quantity": "1"
+}
+
 #Create Graph
 # response = requests.post(url=graph_endpoint,json=graph_config, headers=headers)
 # print(response.text)
 
 #Post
+response = requests.post(url=pixel_create_endpoint,headers=headers,json=pixel_create_config)
+print(response.text)
+
+print(pixel_create_endpoint)
