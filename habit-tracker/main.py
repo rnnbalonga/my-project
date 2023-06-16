9de772b3e6800160d7eff02a2b8786e99aee740c
Nike Balonga <rnnbalongapro@gmail.com>
2023-06-16 21:01:59 +0900

    Create Global var for GRAPH_ID and create post_endpoint

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -2,9 +2,11 @@ import requests
 
 USERNAME = "nikebalonga"
 TOKEN = "29b2f8302a75c6bbbccc0867ba79ad8e"
+GRAPH_ID = "q1c1b0f4b2302ca3"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+post_endpoint = f"/v1/users/{USERNAME}/graphs/{GRAPH_ID}"
 
 user_params = {
     "token": TOKEN,
@@ -14,7 +16,7 @@ user_params = {
 }
 
 graph_config = {
-    "id": "q1c1b0f4b2302ca3",
+    "id": GRAPH_ID,
     "name": "Coding Habit",
     "unit": "Days",
     "type": "int",
@@ -25,5 +27,8 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(url=graph_endpoint,json=graph_config, headers=headers)
-print(response.text)
+#Create Graph
+# response = requests.post(url=graph_endpoint,json=graph_config, headers=headers)
+# print(response.text)
+
+#Post
