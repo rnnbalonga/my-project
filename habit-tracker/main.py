531d2218024f597c5a5bf63a7f51a4798539bd67
Nike Balonga <rnnbalongapro@gmail.com>
2023-06-19 19:46:18 +0900

    Latest ver of project

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -5,7 +5,7 @@ USERNAME = "nikebalonga"
 TOKEN = "29b2f8302a75c6bbbccc0867ba79ad8e"
 GRAPH_ID = "q1c1b0f4b2302ca3"
 
-date = dt.datetime.now()
+date = dt.datetime(2023,6,12)
 date_format = date.strftime("%Y%m%d")
 
 pixela_endpoint = "https://pixe.la/v1/users"
@@ -13,7 +13,7 @@ graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 graph_update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
 pixel_create_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
 pixel_update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{date_format}"
-
+print(f"{graph_endpoint}/{GRAPH_ID}")
 
 
 headers = {
@@ -42,12 +42,7 @@ graph_update_config = {
 
 pixel_post_config = {
     "date" : date_format,
-    "quantity" : input("How many lessons did you do today?: "),
-}
-
-pixel_create_config = {
-    "date" : date.strftime("%Y%m%d"),
-    "quantity": "1"
+    "quantity" : str(input("How many lessons did you do today?: ")),
 }
 
 pixel_update_config = {
@@ -56,22 +51,27 @@ pixel_update_config = {
 
 
 #Create Graph
+
 # response = requests.post(url=graph_endpoint,json=graph_config, headers=headers)
 # print(response.text)
 
 #Update Graph
+
 # response = requests.put(url=graph_update_endpoint, headers=headers, json=graph_update_config)
 # print(response.text)
 
-# Post a Pixel
-# response = requests.post(url=pixel_create_endpoint,headers=headers,json=pixel_create_config)
-# print(response.text)
+#Post a Pixel
 
-#Update Pixel
-#REMINDER: ALWAYS CHECK THE TYPE OF REQUEST
+response = requests.post(url=pixel_create_endpoint,headers=headers,json=pixel_post_config)
+print(response.text)
+
+# Update Pixel
+# REMINDER: ALWAYS CHECK THE TYPE OF REQUEST
+
 # response = requests.put(url=pixel_update_endpoint, headers=headers, json=pixel_update_config)
 # print(response.text)
 
 #Delete a pixel
+
 # response = requests.delete(url=pixel_update_endpoint, headers=headers)
 # print(response.text)
