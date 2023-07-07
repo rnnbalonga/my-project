c8ec2fc15ffe59767201c37ead497f2ce27696d3
Nike Balonga <rnnbalongapro@gmail.com>
2023-07-07 15:52:21 +0900

    Figure out how to target desired elements and print them out

--- mavs-news/main.py
+++ mavs-news/main.py
@@ -8,8 +8,12 @@ soup = BeautifulSoup(source, "lxml")
 
 hero = soup.find('div', class_='c-six-up')
 
-
+#Find target elements
 article = hero.h2.text
-article_link = hero.h2
+article_link = hero.h2.a['href']
+article_author = hero.find('span', class_="c-byline__author-name").text
 print(article)
+print(article_link)
+print(article_author)
 
+#Loop
