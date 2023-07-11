a793d32144a307e1e125d456c4f0ab1b0c5c889a
Nike Balonga <rnnbalongapro@gmail.com>
2023-07-11 22:39:46 +0900

    Create dataframe from scraped article info

--- mavs-news/main.py
+++ mavs-news/main.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-import pandas
+import pandas as pd
 
 response = requests.get("https://www.mavsmoneyball.com/")
 source = response.text
@@ -9,15 +9,18 @@ soup = BeautifulSoup(source, "lxml")
 
 hero = soup.find('div', class_='c-six-up__main')
 
-all_articles = [['Article Title', 'Author', 'Link']]
+article_list = []
 
 for article in hero.find_all('div', class_="c-entry-box--compact__body"):
     article_info = []
-    article_info['article_title'] = article.h2.text
-    article_info['article_author'] = article.find('span', class_="c-byline__author-name").text
-    article_info['article_link'] = article.h2.a['href']
-    
-    all_articles.append(article_info)
+    rticle_title = article_info.append(article.h2.text)
+    article_author = article_info.append(article.find('span', class_="c-byline__author-name").text)
+    article_link = article_info.append(article.h2.a['href'])
+    article_list.append(article_info)
+
+# print(article_list)
+
+all_articles = pd.DataFrame(article_list, columns=['Title', 'Author', 'Link'])
 
 print(all_articles)
 
