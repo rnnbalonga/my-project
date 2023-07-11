09a59e1de787e658d724e2cfeb6f3ba549897fac
Nike Balonga <rnnbalongapro@gmail.com>
2023-07-11 22:13:06 +0900

    Create list 'all articles' and append each article in hero to that list

--- mavs-news/main.py
+++ mavs-news/main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas
 
 response = requests.get("https://www.mavsmoneyball.com/")
 source = response.text
@@ -8,12 +9,17 @@ soup = BeautifulSoup(source, "lxml")
 
 hero = soup.find('div', class_='c-six-up__main')
 
+all_articles = [['Article Title', 'Author', 'Link']]
+
 for article in hero.find_all('div', class_="c-entry-box--compact__body"):
-    article_title = article.h2.text
-    article_link = article.h2.a['href']
-    article_author = article.find('span', class_="c-byline__author-name").text
-    print(article_title)
-    print(article_link)
-    print(article_author)
-    print('\n')
+    article_info = []
+    article_info['article_title'] = article.h2.text
+    article_info['article_author'] = article.find('span', class_="c-byline__author-name").text
+    article_info['article_link'] = article.h2.a['href']
+    
+    all_articles.append(article_info)
+
+print(all_articles)
+
+#Generate a CSV with columns "Article", "Date Published", "Author", "Link"
 
