9648e6f44b3ed50358564d552040d2623b854009
Nike Balonga <rnnbalongapro@gmail.com>
2023-07-18 21:46:48 +0900

    Successfully print list.txt

--- daily-bible/esv-study-plan/main.py
+++ daily-bible/esv-study-plan/main.py
@@ -1,5 +1,5 @@
 path = "D:\workspace\my-project\daily-bible\esv-study-plan\list.txt"
 
-with open(path, 'r') as file:
+with open(path, 'r', encoding="utf8") as file:
     for line in file:
         print(line.strip())  # Use t
