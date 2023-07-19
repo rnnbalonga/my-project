c7c3f5acf48e83d4f64c10aa51eada3b1a668071
Nike Balonga <rnnbalongapro@gmail.com>
2023-07-19 17:55:31 +0900

    Successfully reformat the esv reading plan and produce a txt file

--- daily-bible/esv-study-plan/main.py
+++ daily-bible/esv-study-plan/main.py
@@ -4,10 +4,12 @@ months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", 
 
 
 with open(path, 'r', encoding="utf8") as file:
-    for line in file:
-        line = line.strip()
-        combined_line = ""
-        if not any (word in line for word in months):
-            combined_line += previous_line + " " + line
-            print(combined_line)
-        previous_line = line
+    with open("new_list", 'w', encoding="utf8") as new_file:
+        for line in file:
+            line = line.strip()
+            combined_line = ""
+            if not any (word in line for word in months):
+                combined_line += previous_line + " " + line
+                line = combined_line
+            previous_line = line
+            new_file.write(line + "\n")
