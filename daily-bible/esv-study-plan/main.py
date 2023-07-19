065f5524006a6fe417320e5db0f8494229af20a0
Nike Balonga <rnnbalongapro@gmail.com>
2023-07-19 17:40:39 +0900

    Successfuly join lines without month with the previous line

--- daily-bible/esv-study-plan/main.py
+++ daily-bible/esv-study-plan/main.py
@@ -1,5 +1,13 @@
 path = "D:\workspace\my-project\daily-bible\esv-study-plan\list.txt"
 
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+
 with open(path, 'r', encoding="utf8") as file:
     for line in file:
-        print(line.strip())  # Use t
+        line = line.strip()
+        combined_line = ""
+        if not any (word in line for word in months):
+            combined_line += previous_line + " " + line
+            print(combined_line)
+        previous_line = line
