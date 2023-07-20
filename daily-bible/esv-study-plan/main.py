461be86db33ca8733307b53b598b277ebac30988
Nike Balonga <rnnbalongapro@gmail.com>
2023-07-20 22:40:25 +0900

    Experiment with interaction between two functions

--- daily-bible/esv-study-plan/main.py
+++ daily-bible/esv-study-plan/main.py
@@ -25,17 +25,30 @@ def compare_first_15_char(line_1, line_2):
     This function will compare first 15 characters of each line. Return True or False.
     """
     if line_1[0:14] == line_2[0:14]:
-        return True
+        return line_1, line_2
     else:
         return False
+    
+def compare_str_length(line_1, line_2, lst):
+    """
+    This function will compare the length of two lines and remove the shorter one from the list.
+    """
+    new_list = lst
+    if len(line_1) >= len(line_2):
+        new_list.remove(line_2)
+        print(new_list)
+    else:
+        new_list.remove(line_1)
+        print(new_list)
+    
 
 #Experiment with variables
 sample_line_1 = "Jan 9 Ps 8, Gen 8:20‐9:19, 1 Chr 9,"
 sample_line_2 = "Jan 9 Ps 8, Gen 8:20‐9:19, 1 Chr 9, Luke 5:1‐6:16"
 sample_line_3 = "Jan 10 Ps 9, Gen 9:20‐10:32, 1 Chr 10,"
 
-is_same = compare_first_15_char(sample_line_1, sample_line_2)
-is_same_2 = compare_first_15_char(sample_line_1, sample_line_3)
+# is_same = compare_first_15_char(sample_line_1, sample_line_2)
+# is_same_2 = compare_first_15_char(sample_line_1, sample_line_3)
 # print(is_same) #True
 # print(is_same_2) #False
 
@@ -43,5 +56,5 @@ is_same_2 = compare_first_15_char(sample_line_1, sample_line_3)
 sample_line_list = [sample_line_1, sample_line_2, sample_line_3]
 list_is_same = compare_first_15_char(sample_line_list[0], sample_line_list[1])
 list_is_same_2 = compare_first_15_char(sample_line_list[0], sample_line_list[2])
-print(list_is_same) #True
-print(list_is_same_2) #False
+
+compare_str_length(list_is_same[0], list_is_same[1], sample_line_list)
