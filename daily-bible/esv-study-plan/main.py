33088ede3334902d92054944a9facacf022db3b0
Nike Balonga <rnnbalongapro@gmail.com>
2023-07-27 21:51:43 +0900

    Solve problem of creating the list that removes shorter duplicates of each line in a file.

--- daily-bible/esv-study-plan/main.py
+++ daily-bible/esv-study-plan/main.py
@@ -1,42 +1,53 @@
-def compare_first_15_char(line_1, line_2):
+def compare_first_15_char(prev_line, current_line):
     """
     This function will compare first 15 characters of each line. Return True or False.
     """
-    if line_1[0:14] == line_2[0:14]:
-        return line_1, line_2
+    if current_line[0:14] == prev_line[0:14]:
+        return True
     else:
         return False
     
-def compare_str_length(line_1, line_2, lst):
+def compare_str_length(prev_line, current_line):
     """
-    This function will compare the length of two lines and remove the shorter one from the list.
+    This function will compare the length of two lines.
     """
-    if len(line_1) >= len(line_2):
-        lst.remove(line_2)
-        # print(lst)
+    if len(current_line) > len(prev_line):
+        return current_line
     else:
-        lst.remove(line_1)
-        # print(lst)
+        return prev_line
 
 with open("new_list.txt", 'r', encoding="utf8") as file:
     line_list = file.readlines()
-    clean_list = []
-    #Create clean list
+    raw_list = []
     for i in range(len(line_list)):
         line = line_list[i].strip()
-        clean_list.append(line)
+        raw_list.append(line)
+    # print(len(raw_list))
+        
+clean_list = []
 
-    
-    #Use the two functions to reduce lines in clean_list
-    # for i in range(0,10):
-    #         line_1 = clean_list[i]
-    #         line_2 = clean_list[i+1]
-    #         is_same_15_char = compare_first_15_char(line_1, line_2)
-    #         if not is_same_15_char:
-    #             compare_str_length(line_1, line_2, clean_list)
 
-    print(len(clean_list))
-    print(clean_list[550])
+
+for i in range(0, len(raw_list) - 1):
+    if i == 0:
+        clean_list.append(raw_list[i])
+        # print(clean_list)
+    else:
+        prev_line = clean_list[-1]
+        current_line = raw_list[i]
+
+        is_same = compare_first_15_char(prev_line, current_line)
+
+        if is_same:
+            longer_line = compare_str_length(prev_line, current_line)
+            prev_line = longer_line
+        
+        else:
+            clean_list.append(current_line)
+
+print(len(clean_list)) 
+
+
 ### CODE FOR FORMATTING (NOT NEEDED) ###
 
 # path = "D:\workspace\my-project\daily-bible\esv-study-plan\list.txt"
@@ -54,21 +65,4 @@ with open("new_list.txt", 'r', encoding="utf8") as file:
 #                 line = combined_line
 #             previous_line = line
 #             new_file.write(line + "\n")
-
-### TESTS ###
-
-# #Experiment with variables
-# sample_line_list = ["Jan 9 Ps 8, Gen 8:20‐9:19, 1 Chr 9,", "Jan 9 Ps 8, Gen 8:20‐9:19, 1 Chr 9, Luke 5:1‐6:16", "BROOOOOO"]
-
-# is_same = compare_first_15_char(sample_line_1, sample_line_2)
-# is_same_2 = compare_first_15_char(sample_line_1, sample_line_3)
-# print(is_same) #True
-# print(is_same_2) #False
-
-#Experiment using items in a list
-# list_is_same = compare_first_15_char(sample_line_list[0], sample_line_list[1])
-# list_is_same_2 = compare_first_15_char(sample_line_list[0], sample_line_list[2])
-
-# if list_is_same:
-#     compare_str_length(list_is_same[0], list_is_same[1], sample_line_list)
 
