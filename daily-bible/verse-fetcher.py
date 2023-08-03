f2b2faab99ea95f956fcbeea58e5160089743bac
Nike Balonga <rnnbalongapro@gmail.com>
2023-08-03 21:57:29 +0900

    Create class VerseFetcher, add read_data method, add give_line method

--- daily-bible/verse-fetcher.py
+++ daily-bible/verse-fetcher.py
@@ -1,3 +1,23 @@
 class VerseFetcher:
-    def __init__(self, database) -> None:
-        database = ""
+    # The aim for this class is to generate all the verses needed for a specific day.
+    # Day Counter is not part of this class but would be provided through another class.
+
+    def __init__(self):
+        self.database = self.read_data("esv-study-plan\clean_list.txt")
+
+    def read_data(self, path):
+        """
+        Fetch the items from clean_list.
+        """
+        with open (path, "r", encoding="utf-8") as file:
+            data = [line.strip() for line in file]
+        return data
+    
+    def give_line(self, day):
+        """
+        Give the line that corresponds to the given day.
+        """
+        line = self.database[day]
+        return line
+
+test = VerseFetcher()
