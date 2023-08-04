e92ce852ecc6649dec290e284dbf32b4189c145a
Nike Balonga <rnnbalongapro@gmail.com>
2023-08-04 10:42:43 +0900

    Test new function to add integers

--- daily-bible/discord_bot.py
+++ daily-bible/discord_bot.py
@@ -12,8 +12,16 @@ async def on_ready():
     channel = bot.get_channel(CHANNEL_ID)
     await channel.send("Hello, I'm ready to roll out!")
 
+#Commands Samples
+
 @bot.command()
+#Say Hello
 async def hello(ctx):
     await ctx.send("Hello!")
+
+#Add integers
+async def add(ctx, x, y):
+    sum = int(x) + int(y)
+    await ctx.send(f'{sum}')
 
 bot.run(TOKEN)
