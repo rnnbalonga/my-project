04cb11e2aa9f2d8467f5455ac3c65446076abdc0
Nike Balonga <rnnbalongapro@gmail.com>
2023-08-07 14:46:13 +0900

    Create class DiscordBot. Still not working.

--- daily-bible/discord_bot.py
+++ daily-bible/discord_bot.py
@@ -2,29 +2,32 @@ from discord.ext import commands
 import discord
 import os
 
-TOKEN = os.environ.get("DISCORD_BOT_TOKEN")
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID"))
 
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+class DiscordBot:
+    def __init__(self):
+        self.TOKEN = os.environ.get("DISCORD_BOT_TOKEN")
+        self.CHANNEL_ID = int(os.environ.get("CHANNEL_ID"))
+        self.bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+        self.bot.run(self.TOKEN)
 
-@bot.event
-async def on_ready():
-    print("Hello!")
-    channel = bot.get_channel(CHANNEL_ID)
-    await channel.send("Hello, I'm ready to roll out!")
+    @bot.event
+    async def on_ready(self):
+        print("Hello!")
+        channel = self.bot.get_channel(self.CHANNEL_ID)
+        await channel.send("Hello, I'm ready to roll out!")
 
-#Commands Samples
-#For each command, you need to add the line @bot.command()
+    #Commands Samples
+    #For each command, you need to add the line @bot.command()
 
-@bot.command()
-#Say Hello
-async def hello(ctx):
-    await ctx.send("Hello!")
+    @bot.command()
+    #Say Hello
+    async def hello(ctx):
+        await ctx.send("Hello!")
 
-#Add integers
-@bot.command()
-async def add(ctx, x, y):
-    sum = int(x) + int(y)
-    await ctx.send(f'{sum}')
+    #Add integers
+    @bot.command()
+    async def add(ctx, x, y):
+        sum = int(x) + int(y)
+        await ctx.send(f'{sum}')
 
-bot.run(TOKEN)
+    
