9847d7534f7a5ef0c5a24b794f1dcbb903ba1d23
Nike Balonga <rnnbalongapro@gmail.com>
2023-08-22 22:06:40 +0900

    Import DiscordBot into main file

--- daily-bible/main.py
+++ daily-bible/main.py
@@ -1,10 +1,13 @@
 from day_counter import DayCounter
 from verse_fetcher import VerseFetcher
+from discord_bot import DiscordBot
 
 user_day = DayCounter()
 user_verse = VerseFetcher()
+bot = DiscordBot()
 
 day = user_day.day
 today_verse = user_verse.daily_verse(day)
 for verse in today_verse:
     print(verse)
+
