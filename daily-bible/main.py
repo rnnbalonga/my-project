c0ea061bbc792528878169f4cd9a0b27e20dd1d8
Nike Balonga <rnnbalongapro@gmail.com>
2023-08-25 08:46:51 +0900

    Try something out.

--- daily-bible/main.py
+++ daily-bible/main.py
@@ -1,13 +1,13 @@
 from day_counter import DayCounter
 from verse_fetcher import VerseFetcher
-from discord_bot import DiscordBot
+# from discord_bot import DiscordBot
 
 user_day = DayCounter()
 user_verse = VerseFetcher()
-bot = DiscordBot()
+# bot = DiscordBot()
 
-day = user_day.day
-today_verse = user_verse.daily_verse(day)
-for verse in today_verse:
-    print(verse)
+print(user_day.day)
+user_day.add_day()
+print(user_day.day)
 
+print(user_verse.daily_verse(user_day.day))
