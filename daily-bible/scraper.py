1db1d73b2c1ba4df370e8b0741aac8bddf6328da
Nike Balonga <rnnbalongapro@gmail.com>
2023-09-12 09:57:34 +0900

    Clean up scraper code

--- daily-bible/scraper.py
+++ daily-bible/scraper.py
@@ -1,13 +1,12 @@
 from selenium import webdriver
 
-chrome_driver_path = r"D:\Installers\chromedriver_win32\chromedriver.exe"
 
-#This opens selenium browser
-driver = webdriver.Chrome()
-
-driver.get("https://www.biblegateway.com/versions/English-Standard-Version-ESV-Bible/")
-
-input()
-
-driver.quit()
+class Scraper:
+    def __init__(self):
+        self.chrome_driver_path = r"D:\Installers\chromedriver_win32\chromedriver.exe"
+        #This opens selenium browser
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://www.biblegateway.com/versions/English-Standard-Version-ESV-Bible/")
+        input()
+        self.driver.quit()
 
