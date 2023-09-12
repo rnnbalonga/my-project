6921b1755f11a01780875ebb4bb7306a6e0dc4ec
Nike Balonga <rnnbalongapro@gmail.com>
2023-09-12 09:58:23 +0900

    Add feature to increase user_day.day by 1 every time a new day arrives

--- daily-bible/main.py
+++ daily-bible/main.py
@@ -1,13 +1,23 @@
 from day_counter import DayCounter
 from verse_fetcher import VerseFetcher
+import datetime
+import time
 # from discord_bot import DiscordBot
 
 user_day = DayCounter()
 user_verse = VerseFetcher()
-# bot = DiscordBot()
 
-print(user_day.day)
-user_day.add_day()
-print(user_day.day)
+#Get user_verse list from user_day
+current_verse = user_verse.daily_verse(user_day.day)
+print(f"Current verse: {current_verse}")
 
-print(user_verse.daily_verse(user_day.day))
+#Call user_day.add_day() after 12AM
+current_date = datetime.date.today()
+
+while True:
+    time.sleep(1)
+    new_date = datetime.date.today()
+    if new_date > current_date:
+        user_day.add_day()
+        current_date = new_date 
+
