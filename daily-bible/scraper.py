6bf7f3d6f2b121ae2c11bbe4a358d766572c512e
Nike Balonga <rnnbalongapro@gmail.com>
2023-09-12 15:22:22 +0900

    Create WebScraper class

--- daily-bible/scraper.py
+++ daily-bible/scraper.py
@@ -4,41 +4,32 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-options = ChromeOptions()
-options.add_experimental_option("detach", True)
+class WebScraper:
+    def __init__(self):
+        options = ChromeOptions()
+        options.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), 
+                                options=options)
+    def scrape_verse(self, query):
+    # Navigate to the url
+        self.driver.get('https://www.biblegateway.com/versions/English-Standard-Version-ESV-Bible/')
 
+        # Clear Text from the search bar
+        self.driver.find_element(By.NAME, "quicksearch").clear()
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), 
-                          options=options)
+        # Enter Text
+        self.driver.find_element(By.NAME, "quicksearch").send_keys(query)
 
-# Navigate to the url
-driver.get('https://www.biblegateway.com/versions/English-Standard-Version-ESV-Bible/')
-# driver.maximize_window()
+        # CLick Button
+        self.driver.find_element(By.XPATH, "/html/body/div[2]/div/section/div[1]/div/form/div[1]/div[2]/button").click()
 
-driver.find_element(By.NAME, "quicksearch").clear()
+        #Get the passage location
+        self.verse_ref = self.driver.find_element(By.CLASS_NAME, "dropdown-display-text").text
+        #Get the verse
+        verse = self.driver.find_element(By.CLASS_NAME, "passage-text" ).text
+        print(verse)
 
-	# Enter Text
-driver.find_element(By.NAME, "quicksearch").send_keys("psa 1" )
+        self.driver.quit()
 
-driver.find_element(By.XPATH, "/html/body/div[2]/div/section/div[1]/div/form/div[1]/div[2]/button").click()
-
-verse_ref = driver.find_element(By.CLASS_NAME, "dropdown-display-text").text
-verse = driver.find_element(By.CLASS_NAME, "passage-text" ).text
-print(verse)
-
-driver.quit()
-
-# Close the driver
-# driver.quit()
-
-# class Scraper:
-#     def __init__(self):
-#         self.chrome_driver_path = r"D:\Installers\chromedriver_win32\chromedriver.exe"
-#         #This opens selenium browser
-#         self.driver = webdriver.Chrome()
-#         self.driver.get("https://www.biblegateway.com/versions/English-Standard-Version-ESV-Bible/")
-#         input()
-#         self.quit = self.driver.quit()
-
-# scraper = Scraper()
-# scraper.quit()
+verse_scraper = WebScraper()
+verse_scraper.scrape_verse("Matt 1:1-20")
