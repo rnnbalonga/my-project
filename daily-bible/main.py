cce38e18cf30ac6ade9e99f41cea98cd4a47016b
Nike Balonga <rnnbalongapro@gmail.com>
2023-09-27 22:08:37 +0900

    Test creating a list of dictionary for verses

--- daily-bible/main.py
+++ daily-bible/main.py
@@ -11,18 +11,32 @@ user_verse = VerseFetcher()
 
 #Get user_verse list from user_day
 current_verse = user_verse.daily_verse(user_day.day)
-print(type(current_verse[0]))
+print(current_verse)
+
 
 #Scrape verse from current_verse
 
 
-scraper = WebScraper()
+# scraper = WebScraper()
 
 #Test 1 verse
-# scraper.scrape_verse(current_verse[0])
+#scraper.scrape_verse(current_verse[0])
+
+#Consider saving the scraped verses into a variable which can be accessed later. Create a list of dictionaries where book-chapter is the key and the passage is the value.
+# current_day_output = []
 
 #Loop through all items in list
-#Consider saving the scraped verses into a variable which can be accessed later. Create a dictionary where book-chapter is the key and the passage is the value.
+for verse in current_verse:
+    current_verse_output = {}
+    current_verse_output['reference'] = verse
+    current_verse_output['verse'] = scraper.scrape_verse(verse)
+    current_day_output.append(current_verse_output)
+
+scraper.quit()
+
+print(len(current_day_output))
+
+
 #Consider showing a message in the console that {passage} has been saved.
 #Browser should only close after all passages have been successfully scraped
 
