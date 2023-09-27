e0aa60922a44dc56f5528215a90e4d576153e1ef
Nike Balonga <rnnbalongapro@gmail.com>
2023-09-27 22:22:26 +0900

    Testing successful

--- daily-bible/main.py
+++ daily-bible/main.py
@@ -11,21 +11,21 @@ user_verse = VerseFetcher()
 
 #Get user_verse list from user_day
 current_verse = user_verse.daily_verse(user_day.day)
-print(current_verse)
+print(current_verse[2])
 
 
 #Scrape verse from current_verse
 
 
-# scraper = WebScraper()
+scraper = WebScraper()
 
-#Test 1 verse
-#scraper.scrape_verse(current_verse[0])
+# #Test 1 verse
+# #scraper.scrape_verse(current_verse[0])
 
-#Consider saving the scraped verses into a variable which can be accessed later. Create a list of dictionaries where book-chapter is the key and the passage is the value.
-# current_day_output = []
+# #Consider saving the scraped verses into a variable which can be accessed later. Create a list of dictionaries where book-chapter is the key and the passage is the value.
+current_day_output = []
 
-#Loop through all items in list
+# #Loop through all items in list
 for verse in current_verse:
     current_verse_output = {}
     current_verse_output['reference'] = verse
