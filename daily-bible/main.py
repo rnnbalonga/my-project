d0c52aeca6429a7b2e6ab757669ecdaa838753de
Nike Balonga <rnnbalongapro@gmail.com>
2023-09-28 10:43:09 +0900

    Create scrape_verse function that can be called once a

--- daily-bible/main.py
+++ daily-bible/main.py
@@ -1,46 +1,43 @@
+from datetime import date
 from day_counter import DayCounter
 from verse_fetcher import VerseFetcher
 from scraper import WebScraper
 
-
-# from discord_bot import DiscordBot
-
 user_day = DayCounter()
 user_verse = VerseFetcher()
-
-
-#Get user_verse list from user_day
-current_verse = user_verse.daily_verse(user_day.day)
-print(current_verse[2])
-
-
-#Scrape verse from current_verse
-
-
 scraper = WebScraper()
 
-# #Test 1 verse
-# #scraper.scrape_verse(current_verse[0])
+# Get user_verse list from user_day
 
-# #Consider saving the scraped verses into a variable which can be accessed later. Create a list of dictionaries where book-chapter is the key and the passage is the value.
-current_day_output = []
+def scrape_verse():
+    current_verse = user_verse.daily_verse(user_day.day)
 
-scraper.open_site()
+    #Consider saving the scraped verses into a variable which can be accessed later. Create a list of dictionaries where book-chapter is the key and the passage is the value.
+    current_day_output = []
 
-# #Loop through all items in list
-for verse in current_verse:
-    current_verse_output = {}
-    current_verse_output['reference'] = verse
-    current_verse_output['verse'] = scraper.scrape_verse(verse)
-    current_day_output.append(current_verse_output)
+    scraper.open_site()
 
-scraper.quit()
+    #Loop through all items in list
+    for verse in current_verse:
+        current_verse_output = {}
+        current_verse_output['reference'] = verse
+        current_verse_output['verse'] = scraper.scrape_verse(verse)
+        current_day_output.append(current_verse_output)
 
-print(len(current_day_output))
+    scraper.quit()
 
+    for output in current_day_output:
+        print(output)
+        print("\n")
 
-#Consider showing a message in the console that {passage} has been saved.
-#Browser should only close after all passages have been successfully scraped
+    #Call add_day method when current date is changed
+    current_date = str(date.today())
+    origin_date = ""
 
-# for verse in current_verse:
-#     scraper.scrape_verse(verse)
+    if current_date != origin_date:
+        origin_date = current_date
+        user_day.add_day()
+    else:
+        pass
+
+scrape_verse()
