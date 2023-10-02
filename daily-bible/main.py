ddfc178bba6eca1a2899774c517925f594ff9beb
Nike Balonga <rnnbalongapro@gmail.com>
2023-10-02 09:44:12 +0900

    Simplify code in main function.

--- daily-bible/main.py
+++ daily-bible/main.py
@@ -16,26 +16,13 @@ def main ():
     current_date = str(date.today())
     origin_date = user_day.get_origin_date()
 
-    if not user_day.compare_dates(current_date=current_date, origin_date=origin_date):
+    if current_date != origin_date:
         user_day.store_date(current_date)
         user_day.add_day()
 
-    current_verse = user_verse.daily_verse(user_day.day)
+    current_verse = user_verse.daily_verse(user_day.day) #output -> list
 
-    current_day_output = []
-
-    scraper.open_site()
-
-    current_day_output = [{'reference': verse, 'verse': scraper.scrape_verse(verse)} for verse in current_verse]
-
-    for output in current_day_output:
-        print(output['reference'])
-        print("\n")
-        print(output['verse'])
-        
-    scraper.quit()
-
-    print(current_day_output)
+    scraper.collect_output_verse(current_verse)
 
 
 main()
