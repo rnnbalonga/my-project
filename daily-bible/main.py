5ebdd53ad4a28088991f7a20de150783986fef6a
Nike Balonga <rnnbalongapro@gmail.com>
2023-10-04 21:23:49 +0900

    Add schedule to call the main function everyday at 12AM

--- daily-bible/main.py
+++ daily-bible/main.py
@@ -3,6 +3,8 @@ from day_counter import DayCounter
 from verse_fetcher import VerseFetcher
 from scraper import WebScraper
 from discord.ext import commands
+import schedule
+import time
 import discord
 import os
 
@@ -17,12 +19,20 @@ def main ():
     origin_date = user_day.get_origin_date()
 
     if current_date != origin_date:
+        #Manipulate the data in the last_run_date.txt file
         user_day.store_date(current_date)
         user_day.add_day()
+        #Scrape verse & store output in today_verse.txt
+        current_verse = user_verse.daily_verse(user_day.day) #output -> list
+        scraper.collect_output_verse(current_verse)
+    else:
+        pass
 
-    current_verse = user_verse.daily_verse(user_day.day) #output -> list
+def sched():
+    main()
 
-    scraper.collect_output_verse(current_verse)
+schedule.every().day.at("00:00").do(sched)
 
-
-main()
+while True:
+    schedule.run_pending()
+    time.sleep(60)
