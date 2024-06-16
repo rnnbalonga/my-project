a7ef9b98598397a497311e68629a4d0bdcbd03b1
Nike Balonga <rnnbalongapro@gmail.com>
2024-06-16 17:56:04 +0900

    Nimm game complete

--- nimm-game/main.py
+++ nimm-game/main.py
@@ -18,26 +18,27 @@ def get_player_choice(player):
     This function will get the player's choice.
     """
     while True:
-        player_choice = input(f"Player {player} would you like to remove 1 or 2 stones? ")
-        #Check if player_choice is an int or not.
-        player_choice = is_int(player_choice)
-        break
-    if is_1_or_2(player_choice):
-            return player_choice
-    #Issue is it will only ask player to enter 1 or 2 just once. If the player insists on entering some other int, it will proceed to use that input.
-    #Need to find a solution.
-    else:
-        player_choice = ask_again()
-        return player_choice
+        try:
+            choice = int(input(f'Player {player} would you like to remove 1 or 2 stones? '))
+        except ValueError:
+            choice = ask_again()
+            return choice
+        else:
+            if is_1_or_2(choice):
+                return choice
+            else:
+                choice = ask_again()
+                return choice
+    
 
-def is_int(choice):
-    """
-    Check if player's choice is an integer.
-    """
-    if type(choice) == int:
-        return True
-    else:
-        return False
+# def is_int(choice):
+#     """
+#     Check if player's choice is an integer.
+#     """
+#     if type(choice) == int:
+#         return True
+#     else:
+#         return False
 
 
 def is_1_or_2(choice):
@@ -49,18 +50,25 @@ def is_1_or_2(choice):
     else:
         return False
 
-def check_if_valid(choice):
-    if is_int(choice) and is_1_or_2(choice):
-        return True
-    else:
-        return False
+# def check_if_valid(choice):
+#     if is_int(choice) and is_1_or_2(choice):
+#         return True
+#     else:
+#         return False
 
 def ask_again():
     while True:
-        player_choice = (input("Please enter 1 or 2: "))
-        player_choice = is_int(player_choice)
-        if player_choice == 1 or player_choice == 2:
-            return player_choice
+        try:
+            choice = int(input('Please enter 1 or 2: '))
+            if is_1_or_2(choice):
+                return choice
+            else:
+                choice = ask_again()
+                return choice
+        except ValueError:
+            choice = ask_again()
+            return choice
+
 
 # def main():
 #     """
@@ -74,6 +82,7 @@ def ask_again():
 #         #Ask player how many stones to subtract
 #         print(f'There are {remaining_stones} stones left.')
 #         subtract = get_player_choice(player)
+#         print('\n')
 #         #Substract input from remaining stones
 #         remaining_stones -= subtract
 #         #Switch player
@@ -83,13 +92,17 @@ def ask_again():
 #             player -= 1
 #     print(f"Player {player} wins!")
         
+
+
+import random 
+
 def main():
-    print(check_if_valid(3)) #expected answer False
-    print(check_if_valid('hey')) #expected answer False
-    print(check_if_valid(1)) #expect True
-    print(check_if_valid(2)) #expect True
- 
- 
+    name = "Nike"
+    greet(name)
+    greet('Mia')
+    
+def greet(name_for_greeting):
+    print(f'Hello {name_for_greeting}!')
 
 if __name__ == '__main__':
     main()
